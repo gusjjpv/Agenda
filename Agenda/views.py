4d553561bd0d79fcad20e1370968b95a6e86d3d2
gusDev <joaogustavolima084@gmail.com>
2024-08-23 14:38:21 -0300

    feat(auth): implement user login and logout functionality

--- Agenda/views.py
+++ Agenda/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render, redirect
 from Agenda.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 
-#def index(request):
+# def index(request):
 #    return redirect('/agenda/')
 
+def login_user(request):
+    return render(request, 'login.html')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuario ou Senha Invalido.")
+    return redirect('/')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
-    evento = Evento.objects.all()
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
     dados = {'evento':evento}
     return render(request, 'agenda.html', dados)
