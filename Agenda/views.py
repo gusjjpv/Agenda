dd43d5bd58b04320c074333551a54748e2433dbd
gusDev <joaogustavolima084@gmail.com>
2024-08-23 07:55:06 -0300

    feat(templates): add base templates for Django project

--- Agenda/views.py
+++ Agenda/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from Agenda.models import Evento
 # Create your views here.
+
+#def index(request):
+#    return redirect('/agenda/')
+
+def lista_eventos(request):
+    evento = Evento.objects.all()
+    dados = {'evento':evento}
+    return render(request, 'agenda.html', dados)
